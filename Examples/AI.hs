5bef966bfc6d07b867f8163a86edb1d9da7048db
Kathleen <kathleen.fisher@gmail.com>
2011-05-11 20:23:12 -0400

    minor changes to AI.hs format

--- Examples/AI.hs
+++ Examples/AI.hs
@@ -21,34 +21,39 @@ import qualified Data.ByteString.Lazy.Char8 as B
       ' ',  response   :: Response,
       ' ',  contentLen :: ContentLength }
       
-  data Src = Addr IP | Name Host  
-  type IP = (Int, '.', Int, '.', Int, '.', Int)
+  data Src = Addr IP_v4 | Name Host  
+  type IP_v4 = (IPInt, '.', IPInt, '.', IPInt, '.', IPInt)
   type Host = StringC ' '
+  type IPInt = constrain i :: Int where <| 0 <= i && i < 256 |>   
 
   data ID = Missing '-' | Id (StringC ' ')
 
   type TimeStamp = ('[', Date, ':', Time, ' ', TimeZone, ']')   
 
-  data Date = Data {day::Int, '/', month::Month, '/', year::Int}    
+  data Date = Date {day::Day, '/', month::Month, '/', year::Int}    
   data Time = Time {hours::Hours, ':', minutes :: Minutes, ':', seconds :: Seconds}  
   type TimeZone = ('-', Int)
 
+  type Year    = constrain y :: Int where <| 1900 <= y && y < 3000 |>   
   data Month   = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec  
+  type Day     = constrain d :: Int where <| 1 <= d && d < 31 |>   
   type Hours   = constrain h :: Int where <| 0 <= h && h < 24 |>   
   type Minutes = constrain m :: Int where <| 0 <= m && m < 60 |> 
   type Seconds = constrain s :: Int where <| 0 <= s && s < 60 |> 
 
-
   data Request = Request 
       { '"',  method  :: Method,       
         ' ',  url     :: StringC ' ', 
-        ' ',  version :: Version  where <| checkVersion method version |>,
-        '"'
+        ' ',  version :: Version  where <| checkVersion method version |>,  '"'
       }  
 
   data Method  = GET | PUT | POST | HEAD | DELETE
                | LINK | UNLINK      -- obsolete after http 1.0
   data Version = Version {"HTTP/", major :: Int, '.', minor :: Int}
+
+  type Response = constrain r :: Int where <| 100 <= r && r < 600 |> 
+
+  data ContentLength = NotAvailable '-' | ContentLength Int 
   |]
 
 checkVersion :: Method -> Version -> Bool
@@ -58,14 +63,8 @@ checkVersion method version =
     UNLINK -> major version == 1 && minor version == 0
     _ -> True
 
-[pads|
-  type Response = constrain r :: Int where <| 100 <= r && r < 600 |> 
 
-  data ContentLength = NotAvailable '-' | ContentLength Int 
-  |]
-
-
---mkPrettyInstance ''AI
+mkPrettyInstance ''AI
 --mkPrettyInstance ''AI_md
 
 ai_file = ai_big
@@ -78,6 +77,8 @@ ai_big  = "Examples/data/ai.big"
 
 ai_file_length  = Prelude.length ai_rep
 ai_file_take n  = Prelude.take n ai_rep
+
+printAI n = putStrLn(pretty 100 (ppr (ai_file_take n)))
 
 {-
 result n  = do 
